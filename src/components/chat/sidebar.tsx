fc87a767a747a29082569ca1577264154bce3186
Vadym <vadym_maslovskyi@pecodesoftware.com>
2024-04-12 14:16:53 +0300

    Chat Page setup

--- src/components/chat/sidebar.tsx
+++ src/components/chat/sidebar.tsx
@@ -1,117 +1,143 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { ChevronDown, ChevronUp, ChevronsDown, ChevronsUpDownIcon, MessageSquare, PlusCircle } from "lucide-react";
-import Cognix from "@/assets/svgs/cognix.svg?react"
-import ChatSquare from "@/assets/svgs/chat-square.svg?react"
+import { ChevronDown, ChevronUp, PlusCircle } from "lucide-react";
+import Cognix from "@/assets/svgs/cognix.svg?react";
+import ChatSquare from "@/assets/svgs/chat-square.svg?react";
+import SideBarIcon from "@/assets/svgs/sidebar-icon.svg?react";
 
 export function SideBar() {
+  const chats = [
+    {
+      id: 1,
+      text: "Enabling “Get Attention”",
+    },
+    {
+      id: 2,
+      text: "Enabling “Get Attention”",
+    },
+    {
+      id: 3,
+      text: "Enabling “Get Attention”",
+    },
+    {
+      id: 4,
+      text: "Collaboard “Get Attention”123123123",
+    },
+    {
+      id: 5,
+      text: "Enabling “Get Attention”",
+    },
+  ];
 
-const chats = [
-  {
-    "id": 1,
-    "text": "Enabling “Get Attention”"
-  },
-  {
-    "id": 2,
-    "text": "Enabling “Get Attention”"
-  },
-  {
-    "id": 3,
-    "text": "Enabling “Get Attention”"
-  },
-  {
-    "id": 4,
-    "text": "Collaboard “Get Attention”123123123"
-  },
-  {
-    "id": 5,
-    "text": "Enabling “Get Attention”"
-  }
-]
+  const settings = [
+    {
+      id: 1,
+      text: "Connectors",
+    },
+    {
+      id: 2,
+      text: "Feedback",
+    },
+    {
+      id: 3,
+      text: "LLMs",
+    },
+    {
+      id: 4,
+      text: "Embeddings",
+    },
+    {
+      id: 4,
+      text: "Users",
+    },
+  ];
 
-const settings = [
-  {
-    "id": 1,
-    "text": "WPF Textbox Data Bind"
-  },
-  {
-    "id": 2,
-    "text": "Enabling “Get Attention”"
-  },
-  {
-    "id": 3,
-    "text": "Gen AI server role in Da232asfasfasf3"
-  },
-  {
-    "id": 4,
-    "text": "Collaboard “Get Attention”"
-  }
-]
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [isHistoryOpen, setIsHistoryOpen] = useState<boolean>(false);
+  const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
 
-const [isHistoryOpen, setIsHistoryOpen] = useState<boolean>(false);
-const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
-
-
-  return (
-<div className='fixed bottom-0 left-0 h-full sm:w-1/6 bg-foreground text-white'>
+  return isSidebarOpen ? (
+    <div className="motion-safe fixed w-48 bg-foreground text-white transition ease-in-out delay-1000">
       <div className="ml-2 mr-2 space-y-3">
-        <div className='space-y-9'>
-      <Cognix className='m-5'/>
-        <div className="mb-4 space-y-5">
-          <Button
-            variant='outline'
-            size='lg'
-            className="shadow-none bg-primary w-full"
-            type="button"
+        <div className="space-y-9">
+          <Cognix className="mt-8" />
+          <SideBarIcon
+            className="absolute top-0 right-0 mr-2"
+            onClick={() => {
+              setSidebarOpen(!isSidebarOpen);
+            }}
+          />
+          <div className="mb-4 space-y-5">
+            <Button
+              variant="outline"
+              size="lg"
+              className="shadow-none bg-primary w-full"
+              type="button"
+            >
+              <PlusCircle className="h-4 w-4 mr-2" />
+              New chat
+            </Button>
+          </div>
+          <div
+            className="flex items-center cursor-pointer"
+            onClick={() => {
+              setIsHistoryOpen(!isHistoryOpen);
+            }}
           >
-            <PlusCircle className="h-4 w-4 mr-2"/>
-            Add new chat
-          </Button>
-        </div>
-        <div 
-          className="flex items-center cursor-pointer"
-          onClick={() => {setIsHistoryOpen(!isHistoryOpen)}}>
-          { isHistoryOpen 
-          ? <ChevronUp className='h-4 w-4'/>
-          : <ChevronDown className='h-4 w-4'/>}
+            {isHistoryOpen ? (
+              <ChevronUp className="h-4 w-4" />
+            ) : (
+              <ChevronDown className="h-4 w-4" />
+            )}
             <span className="ml-2">Chat history</span>
-            </div>
-            </div>
-          { isHistoryOpen && (
-            <div className="flex flex-col ml-2 space-y-2">
-              {chats.map((chat) => (
-              <div 
-                key={chat.id}
-                className="flex flex-row items-center">
-                <ChatSquare className='h-4 w-4 mr-2 flex-shrink-0'/>
-              <span className='truncate'>{chat.text}</span>
+          </div>
+        </div>
+        {isHistoryOpen && (
+          <div className="flex flex-col ml-1 space-y-3 text-sm font-thin text-muted">
+            {chats.map((chat) => (
+              <div key={chat.id} className="flex flex-row items-center">
+                <span className="truncate">{chat.text}</span>
               </div>
-          ))}
-            </div>
-          )
-          }
-          <div 
+            ))}
+          </div>
+        )}
+        <div
           className="flex items-center cursor-pointer pt-3"
-          onClick={() => {setIsSettingsOpen(!isSettingsOpen)}}>
-          { isSettingsOpen 
-          ? <ChevronUp className='h-4 w-4'/>
-          : <ChevronDown className='h-4 w-4'/>}
-            <span className="ml-2">Chat history</span>
-            </div>
-          { isSettingsOpen && (
-            <div className="flex flex-col ml-2 space-y-2">
-              {settings.map((setting) => (
-              <div 
-                key={setting.id}
-                className="flex flex-row items-center">
-                <ChatSquare className='h-4 w-4 mr-2 flex-shrink-0'/>
-              <span className='truncate'>{setting.text}</span>
-              </div>
-          ))}
-            </div>
-          )
-          }
+          onClick={() => {
+            setIsSettingsOpen(!isSettingsOpen);
+          }}
+        >
+          {isSettingsOpen ? (
+            <ChevronUp className="h-4 w-4" />
+          ) : (
+            <ChevronDown className="h-4 w-4" />
+          )}
+          <span className="ml-2">Settings</span>
         </div>
+        {isSettingsOpen && (
+          <div className="flex flex-col ml-4 space-y-3 text-muted">
+            {settings.map((setting) => (
+              <div key={setting.id} className="flex flex-row items-center">
+                <ChatSquare className="h-4 w-4 mr-2 flex-shrink-0" />
+                <span className="truncate">{setting.text}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  ) : (
+    <div className="fixed w-10 bg-foreground text-white">
+      <div className="mt-9 mr-2 space-y-3">
+        <div className="space-y-9">
+          <SideBarIcon
+            className="absolute right-0 rotate-180"
+            onClick={() => {
+              setSidebarOpen(!isSidebarOpen);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         xl: "h-12 w-80 rounded-md px-12",
-        icon: "h-9 w-9 rounded-full",
+        icon: "h-12",
       },
     },
     defaultVariants: {

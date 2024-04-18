7134f75135d08b4da99f7309898a84b01f4caf1b
Vadym <vadym_maslovskyi@pecodesoftware.com>
2024-04-18 16:45:12 +0300

    Login setup

--- src/pages/login/redirect.tsx
+++ src/pages/login/redirect.tsx
@@ -1,25 +1,25 @@
 import { api } from "@/lib/api";
+import { useLocalStorage } from "@/lib/local-store";
 import { useEffect } from "react";
 
+const { set } = useLocalStorage()
 
-export function RedirectComponent() {
-    async function getAccessToken() {
-        console.log("Called login")
-        const accessToken = await api.get(
+function RedirectComponent() {
+    
+    useEffect(() => {
+        api.get(
             `${import.meta.env.VITE_PLATFORM_API_URL}/auth/google/callback?${window.location.href.split("?")[1]}`
           ).then( response => {
             if (response.status === 200) {
-              return response.data
+              response.data
+              set("access_token", response.data.data)
             }
             return ""
           }
+        ).catch( e => {
+            console.log(e)
+        }
         )
-
-    }
-    
-    
-    useEffect(() => {
-        getAccessToken();
       }, []);
     
 

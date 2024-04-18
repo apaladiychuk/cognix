3f73689b264bbc76beb72046898259bdbc5db537
aPaladiychuk <andrey.paladiychuk@gmail.com>
2024-04-18 10:43:42 +0300

    implement jet stream

--- backend/core/messaging/nats.go
+++ backend/core/messaging/nats.go
@@ -2,12 +2,15 @@ package messaging
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/nats-io/nats.go"
-	"github.com/nats-io/nats.go/jetstream"
 	_ "github.com/nats-io/nats.go/jetstream"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 	_ "go.opentelemetry.io/otel/propagation"
+	"go.uber.org/zap"
+	"log"
 
 	_ "go.opentelemetry.io/otel"
 )
@@ -18,11 +21,22 @@ type Client interface {
 }
 
 type client struct {
-	conn *nats.Conn
+	conn                *nats.Conn
+	stream              nats.JetStreamContext
+	connectorStreamName string
 }
 
 func (c *client) Publish(ctx context.Context, topic string, body interface{}) error {
-	message := c.buildMessage(ctx, body)
+	message, err := c.buildMessage(ctx, body)
+	if err != nil {
+		return err
+	}
+	pubAck, err := c.stream.Publish(fmt.Sprintf("%s.%s", c.connectorStreamName, topic), message)
+	if err != nil {
+		return err
+	}
+	zap.S().Infof("Published message with ack: %s", pubAck.Domain)
+	return nil
 }
 
 func (c *client) Listen(ctx context.Context, topic string) (<-chan *Message, error) {
@@ -30,13 +44,14 @@ func (c *client) Listen(ctx context.Context, topic string) (<-chan *Message, err
 	panic("implement me")
 }
 
-func (c *client) buildMessage(ctx context.Context, body interface{}) *Message {
+func (c *client) buildMessage(ctx context.Context, body interface{}) ([]byte, error) {
 	data := make(propagation.MapCarrier)
 	otel.GetTextMapPropagator().Inject(ctx, &data)
-	return &Message{
+	msg := &Message{
 		Header: data,
 		Body:   body,
 	}
+	return json.Marshal(msg)
 }
 
 func NewClient(cfg *Config) (Client, error) {
@@ -49,11 +64,28 @@ func NewClient(cfg *Config) (Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	js, err := jetstream.New(conn)
+	js, err := conn.JetStream(nats.PublishAsyncMaxPending(streamMaxPending))
 	if err != nil {
 		return nil, err
 	}
+
+	stream, err := js.StreamInfo(cfg.ConnectorStreamName)
+	// stream not found, create it
+	if stream == nil {
+		log.Printf("Creating stream: %s\n", cfg.ConnectorStreamName)
+
+		_, err = jetStream.AddStream(&nats.StreamConfig{
+			Name:     cfg.ConnectorStreamName,
+			Subjects: []string{cfg.ConnectorStreamName + ".*"},
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &client{
-		conn: conn,
+		conn:                conn,
+		stream:              js,
+		connectorStreamName: cfg.ConnectorStreamName,
 	}, nil
 }

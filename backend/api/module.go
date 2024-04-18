f06398c728795b48601d41fd90b0122f93db988e
aPaladiychuk <andrey.paladiychuk@gmail.com>
2024-04-18 13:16:43 +0300

    test build

--- backend/api/module.go
+++ backend/api/module.go
@@ -88,7 +88,7 @@ func RunServer(cfg *Config, router *gin.Engine) {
 	srv := http.Server{}
 	srv.Addr = fmt.Sprintf("0.0.0.0:%d", cfg.Port)
 	srv.Handler = router
-	otelzap.S().Infof("Start HTTP server %s ", srv.Addr)
+	otelzap.S().Infof("Start server %s ", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		otelzap.S().Errorf("HTTP server: %s", err.Error())
 	}

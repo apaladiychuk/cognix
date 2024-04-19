00da4d37e9c2c887ed1e2a11ac8c03c01317a7f7
OlehLitvin <oleh_litvin@pecodesoftware.com>
2024-04-20 00:17:49 +0300

    API proxy setup

---
+++ src/lib/axios.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+axios.interceptors.request.use(
+  config => {
+    const token = JSON.parse(localStorage.getItem("access_token") as string)
+    config.headers['Authorization'] = 'Bearer ' + token
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['Accept'] = 'application/json';
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)

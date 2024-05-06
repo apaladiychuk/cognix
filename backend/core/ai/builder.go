d9b566e050126e018f226f25f14b8e77b2487a30
aPaladiychuk <andrey.paladiychuk@gmail.com>
2024-05-06 17:47:49 +0300

    invalidate llm model after user update it

--- backend/core/ai/builder.go
+++ backend/core/ai/builder.go
@@ -1,9 +1,13 @@
 package ai
 
-import "cognix.ch/api/v2/core/model"
+import (
+	"cognix.ch/api/v2/core/model"
+	"sync"
+)
 
 type Builder struct {
 	clients map[int64]OpenAIClient
+	mx      sync.Mutex
 }
 
 func NewBuilder() *Builder {
@@ -11,10 +15,17 @@ func NewBuilder() *Builder {
 }
 
 func (b *Builder) New(llm *model.LLM) OpenAIClient {
+	b.mx.Lock()
+	defer b.mx.Unlock()
 	if client, ok := b.clients[llm.ID.IntPart()]; ok {
 		return client
 	}
 	client := NewOpenAIClient(llm)
 	b.clients[llm.ID.IntPart()] = client
 	return client
+}
+func (b *Builder) Invalidate(llm *model.LLM) {
+	b.mx.Lock()
+	delete(b.clients, llm.ID.IntPart())
+	b.mx.Unlock()
 }

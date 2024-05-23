0b292289230c3165714aa8ba0131a21e869126d1
apCognixCh <ap@cognix.ch>
2024-05-23 14:21:58 +0300

    debug nats streaming

--- src/backend/golang/core/messaging/nats_stream.go
+++ src/backend/golang/core/messaging/nats_stream.go
@@ -10,7 +10,6 @@ import (
 	_ "github.com/nats-io/nats.go/jetstream"
 	"go.uber.org/zap"
 	"sync"
-	"time"
 )
 
 type clientStream struct {
@@ -18,14 +17,15 @@ type clientStream struct {
 	js                  jetstream.JetStream
 	stream              jetstream.Stream
 	connectorStreamName string
-	subscriptions       []*Subscription
-	once                sync.Once
 	cancel              context.CancelFunc
+	ctx                 context.Context
+	wg                  *sync.WaitGroup
 }
 
 func (c *clientStream) Close() {
+	c.wg.Add(1)
 	c.cancel()
-
+	c.wg.Wait()
 }
 
 func (c *clientStream) Publish(ctx context.Context, topic string, body *proto.Body) error {
@@ -34,52 +34,96 @@ func (c *clientStream) Publish(ctx context.Context, topic string, body *proto.Bo
 		return err
 	}
 	// todo here we must define
-	pubAck, err := c.js.Publish(ctx, fmt.Sprintf("%s.%s", c.connectorStreamName, topic), message)
+	_, err = c.js.Publish(ctx, fmt.Sprintf("%s.%s", c.connectorStreamName, topic), message)
 	//,
 	//		nats.AckWait(time.Minute*2)
 	if err != nil {
 		return err
 	}
-	zap.S().Infof("Published message with ack: %s - %s - %d %v", pubAck.Stream, pubAck.Domain, pubAck.Sequence, pubAck.Duplicate)
 	return nil
 }
 
-func (c *clientStream) Listen(_ context.Context, topic, subscriptionName string, handler MessageHandler) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	c.cancel = cancel
-	cons, _ := c.stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
-		Name:          subscriptionName,
-		FilterSubject: fmt.Sprintf("%s.%s", c.connectorStreamName, topic),
-		AckPolicy:     jetstream.AckAllPolicy,
-		AckWait:       10 * time.Minute,
-		MaxWaiting:    1,
-	})
-	cc, _ := cons.Consume(func(msg jetstream.Msg) {
+func (c *clientStream) Listen(ctx context.Context, topic, subscriptionName string, handler MessageHandler) error {
+
+	//cons, _ := c.stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+	//	Durable:       "subscriptionName",
+	//	FilterSubject: fmt.Sprintf("%s.%s", c.connectorStreamName, topic),
+	//	AckPolicy:     jetstream.AckExplicitPolicy,
+	//	AckWait:       time.Minute,
+	//	MaxWaiting:    1,
+	//})
+	//
+	//cons.Consume(func(msg jetstream.Msg) {
+	//	zap.S().Infof("Received message: %s %s ", msg.Subject(), msg.Reply())
+	//	err := msg.Ack()
+	//	if err != nil {
+	//		zap.S().Errorf("Error acknowledging message: %s", err.Error())
+	//	}
+	//	var message proto.Message
+	//	if err := proto2.Unmarshal(msg.Data(), &message); err != nil {
+	//		zap.S().Errorf("Error unmarshalling message: %s", err.Error())
+	//		return
+	//	}
+	//	if err := handler(ctx, &message); err != nil {
+	//		zap.S().Errorf("Error handling message: %s", err.Error())
+	//	}
+	//	//err := msg.DoubleAck(ctx)
+	//
+	//})
+	_, _ = c.conn.QueueSubscribe(fmt.Sprintf("%s.%s", c.connectorStreamName, topic), "event-processor", func(msg *nats.Msg) {
 		zap.S().Infof("Received message: %s", msg.Reply)
 		var message proto.Message
-		if err := proto2.Unmarshal(msg.Data(), &message); err != nil {
+		if err := proto2.Unmarshal(msg.Data, &message); err != nil {
 			zap.S().Errorf("Error unmarshalling message: %s", err.Error())
 			return
 		}
 		if err := handler(ctx, &message); err != nil {
 			zap.S().Errorf("Error handling message: %s", err.Error())
 		}
-		zap.S().Infof("do ack")
 		err := msg.Ack()
 		if err != nil {
 			zap.S().Errorf("Error acknowledging message: %s", err.Error())
 		}
 	})
-	for {
-		select {
-		case <-ctx.Done():
-			break
-		default:
 
-		}
-	}
-	cc.Stop()
+	//cons, _ := c.stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+	//	Durable:       "subscriptionName",
+	//	FilterSubject: fmt.Sprintf("%s.%s", c.connectorStreamName, topic),
+	//	AckPolicy:     jetstream.AckExplicitPolicy,
+	//	AckWait:       10 * time.Minute,
+	//	MaxWaiting:    1,
+	//})
+
+	//cc, _ := cons.Consume(func(msg jetstream.Msg) {
+	//	zap.S().Infof("Received message: %s", msg.Reply)
+	//	var message proto.Message
+	//	if err := proto2.Unmarshal(msg.Data(), &message); err != nil {
+	//		zap.S().Errorf("Error unmarshalling message: %s", err.Error())
+	//		return
+	//	}
+	//	if err := handler(ctx, &message); err != nil {
+	//		zap.S().Errorf("Error handling message: %s", err.Error())
+	//	}
+	//	zap.S().Infof("do ack")
+	//	err := msg.Ack()
+	//	if err != nil {
+	//		zap.S().Errorf("Error acknowledging message: %s", err.Error())
+	//	}
+	//})
+	//for {
+	//	select {
+	//	case <-ctx.Done():
+	//		break
+	//	default:
+	//
+	//	}
+	//}
+	<-c.ctx.Done()
+	//cc.Stop()
+
+	//c.js.DeleteConsumer(ctx, c.connectorStreamName, "subscriptionName")
 	zap.S().Info("finish")
+	c.wg.Done()
 	return nil
 }
 
@@ -102,19 +146,23 @@ func NewClientStream(cfg *natsConfig) (Client, error) {
 	}
 
 	stream, err := js.CreateOrUpdateStream(context.Background(), jetstream.StreamConfig{
-		Name:     cfg.ConnectorStreamName,
-		Subjects: []string{cfg.ConnectorStreamName + ".>"},
+		Name:      cfg.ConnectorStreamName,
+		Retention: jetstream.WorkQueuePolicy,
+		Storage:   jetstream.FileStorage,
+		Subjects:  []string{cfg.ConnectorStreamName + ".>"},
 	})
 	if err != nil {
 		zap.S().Errorf("Error creating stream: %s", err.Error())
 		return nil, err
 	}
-
+	ctx, cancel := context.WithCancel(context.Background())
 	return &clientStream{
 		conn:                conn,
 		stream:              stream,
 		js:                  js,
 		connectorStreamName: cfg.ConnectorStreamName,
-		once:                sync.Once{},
+		ctx:                 ctx,
+		cancel:              cancel,
+		wg:                  &sync.WaitGroup{},
 	}, nil
 }

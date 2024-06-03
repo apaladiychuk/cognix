992e9c545eebdf23a79fdced951f8b307cdb8a40
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2024-06-03 23:35:03 +0200

    work on cockroach

--- src/backend/semantic/lib/db/db_document.py
+++ src/backend/semantic/lib/db/db_document.py
@@ -20,6 +20,11 @@ class Document(Base):
     creation_date = Column(TIMESTAMP(timezone=False), nullable=False, default=func.now())
     last_update = Column(TIMESTAMP(timezone=False), nullable=True)
 
+    def __repr__(self):
+        return (f"<Document(id={self.id}, parent_id={self.parent_id}, connector_id={self.connector_id}, "
+                f"source_id={self.source_id}, url={self.url}, signature={self.signature}, "
+                f"chunking_session={self.chunking_session}, analyzed={self.analyzed}, "
+                f"creation_date={self.creation_date}, last_update={self.last_update})>")
 
 class DocumentCRUD:
     def __init__(self, connection_string):
@@ -28,20 +33,21 @@ class DocumentCRUD:
         self.session = Session()
         Base.metadata.create_all(self.engine)
 
-    def insert_document(self, **kwargs):
+    def insert_document(self, **kwargs) -> int:
         new_document = Document(**kwargs)
         self.session.add(new_document)
         self.session.commit()
         return new_document.id
 
-    def select_document(self, document_id):
+    def select_document(self, document_id) -> Document | None:
         return self.session.query(Document).filter_by(id=document_id).first()
 
-    def update_document(self, document_id, **kwargs):
-        self.session.query(Document).filter_by(id=document_id).update(kwargs)
+    def update_document(self, document_id, **kwargs) -> int:
+        updated_docs = self.session.query(Document).filter_by(id=document_id).update(kwargs)
         self.session.commit()
+        return updated_docs
 
-    def delete_document(self, document_id):
-        self.session.query(Document).filter_by(id=document_id).delete()
+    def delete_document(self, document_id) -> int:
+        deleted_docs = self.session.query(Document).filter_by(id=document_id).delete()
         self.session.commit()
-
+        return deleted_docs

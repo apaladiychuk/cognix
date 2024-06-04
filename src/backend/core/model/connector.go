4b025f1ac1ebea0d2fce9e287e1d9d13079d3454
apCognixCh <ap@cognix.ch>
2024-06-04 13:16:12 +0300

    remove credentials table. modify connectors table. debug onedrive connector.

--- src/backend/core/model/connector.go
+++ src/backend/core/model/connector.go
@@ -25,21 +25,18 @@ const (
 type Connector struct {
 	tableName               struct{}             `pg:"connectors"`
 	ID                      decimal.Decimal      `json:"id,omitempty"`
-	CredentialID            decimal.NullDecimal  `json:"credential_id,omitempty"`
 	Name                    string               `json:"name,omitempty"`
 	Type                    SourceType           `json:"source,omitempty" pg:"type"`
 	ConnectorSpecificConfig JSONMap              `json:"connector_specific_config,omitempty"`
 	RefreshFreq             int                  `json:"refresh_freq,omitempty"`
 	UserID                  uuid.UUID            `json:"user_id,omitempty"`
 	TenantID                uuid.NullUUID        `json:"tenant_id,omitempty"`
-	Disabled                bool                 `json:"disabled,omitempty" pg:",use_zero"`
 	LastSuccessfulIndexDate pg.NullTime          `json:"last_successful_index_date,omitempty" pg:",use_zero"`
-	LastAttemptStatus       string               `json:"last_attempt_status,omitempty"`
+	Status                  string               `json:"status,omitempty"`
 	TotalDocsIndexed        int                  `json:"total_docs_indexed" pg:",use_zero"`
 	CreationDate            time.Time            `json:"creation_date,omitempty"`
 	LastUpdate              pg.NullTime          `json:"last_update,omitempty" pg:",use_zero"`
 	DeletedDate             pg.NullTime          `json:"deleted_date,omitempty" pg:",use_zero"`
-	Credential              *Credential          `json:"credential,omitempty" pg:"rel:has-one,fk:credential_id"`
 	Docs                    []*Document          `json:"docs,omitempty" pg:"rel:has-many"`
 	DocsMap                 map[string]*Document `json:"docs_map,omitempty" pg:"-"`
 	User                    *User                `json:"-" pg:"rel:has-one,fk:user_id"`

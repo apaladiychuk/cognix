4c20ba5a6dcb725bb3de4e17975940f663c662b9
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2024-06-09 22:30:39 +0200

    work on semantic

--- src/backend/semantic/lib/semantic/semantic_doc.py
+++ src/backend/semantic/lib/semantic/semantic_doc.py
@@ -4,8 +4,7 @@ from lib.semantic.semantic_base import BaseSemantic
 
 class DOCXSemantic(BaseSemantic):
     def analyze(self, data: SemanticData, full_process_start_time: float, ack_wait: int, cockroach_url: str) -> int:
-        # Implement DOCX chunking logic here
-        print(f"Chunking DOCX file: {data}")
-        return 0
+        self.analyze_doc(data=data, full_process_start_time=full_process_start_time, ack_wait=ack_wait,
+                         cockroach_url=cockroach_url)
 
 

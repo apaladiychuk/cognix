cd06c774974dcac6e0fa6fa144c1ad098773a5df
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2024-06-19 15:04:56 +0200

    updated semantic proto

--- src/backend/semantic/lib/semantic/semantic_factory.py
+++ src/backend/semantic/lib/semantic/semantic_factory.py
@@ -5,28 +5,23 @@ from lib.gen_types.semantic_data_pb2 import FileType
 from lib.semantic.semantic_youtube import YTSemantic
 
 
-# Plaintext	.eml, .html, .md, .msg, .rst, .rtf, .txt, .xml
-# Documents	.csv, .doc, .docx, .epub, .odt, .pdf, .ppt, .pptx, .tsv, .xlsx
-
 class SemanticFactory:
+    # Define ranges for connector file types
+    generic_semantic_type_range = range(FileType.UNKNOWN, FileType.MD + 1)
+
     factories = {
         FileType.URL: URLSemantic,
-        FileType.PDF: GenericSemantic,
-        FileType.DOC: GenericSemantic,
-        FileType.TXT: GenericSemantic,
-        FileType.MD: GenericSemantic,
         FileType.YT: YTSemantic,
-        # Add other mappings here
+        # Additional mappings can be added here
     }
 
     @staticmethod
     def create_semantic_analyzer(file_type: FileType) -> BaseSemantic:
-        method_name = SemanticFactory.factories.get(file_type)
-        if not method_name:
-            raise ValueError(f"No factory method found for file type: {file_type}")
+        if file_type in SemanticFactory.generic_semantic_type_range:
+            return GenericSemantic()
+
         semantic_class = SemanticFactory.factories.get(file_type)
         if not semantic_class:
-            raise ValueError(f"Unsupported file type")
+            raise ValueError(f"Unsupported file type: {file_type}")
 
         return semantic_class()
-

43b6d58dda8aa6bc07773091e90cfd7733cee8f4
apCognixCh <ap@cognix.ch>
2024-06-21 18:22:35 +0300

    add new whisper channel

--- src/backend/core/messaging/module.go
+++ src/backend/core/messaging/module.go
@@ -33,6 +33,8 @@ type (
 		ConnectorStreamSubject string `env:"NATS_CLIENT_CONNECTOR_STREAM_SUBJECT,required"`
 		SemanticStreamName     string `env:"NATS_CLIENT_SEMANTIC_STREAM_NAME,required"`
 		SemanticStreamSubject  string `env:"NATS_CLIENT_SEMANTIC_STREAM_SUBJECT,required"`
+		WhisperStreamName      string `env:"NATS_CLIENT_WHISPER_STREAM_NAME,required"`
+		WhisperStreamSubject   string `env:"NATS_CLIENT_WHISPER_STREAM_SUBJECT,required"`
 		AckWait                int    `env:"NATS_CLIENT_CONNECTOR_ACK_WAIT,required"`
 		MaxDeliver             int    `env:"NATS_CLIENT_CONNECTOR_MAX_DELIVER,required"`
 	}

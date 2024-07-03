4bc5e6139a974f4871261c7203cc553387e28d3a
apCognixCh <ap@cognix.ch>
2024-07-03 13:24:38 +0300

    add grpc vector search service

--- src/backend/core/ai/searcher.go
+++ src/backend/core/ai/searcher.go
@@ -10,28 +10,43 @@ import (
 	"time"
 )
 
-// VectorSearchConfig is a configuration struct for vector search module.
-//
-// It contains the configuration options for vector search API,
-// including the endpoint for vector search service.
 type VectorSearchConfig struct {
 	VectorSearchHost string `env:"VECTOR_SEARCHER_GRPC_HOST,required"`
 	VectorSearchPort int    `env:"VECTOR_SEARCHER_GRPC_PORT,required"`
-	ApiVectorSearch  string `env:"API-VECTOR-SEARCH" envDefault:"INTERNAL"`
 }
 
+// GRPCEmbeddingBuilder is a struct that represents a builder for gRPC embedding functionality.
+// It has a configuration, a client, and a mutex for concurrency safety.
 type GRPCEmbeddingBuilder struct {
 	cfg    *VectorSearchConfig
 	client proto.SearchServiceClient
 	mx     sync.Mutex
 }
 
+// NewGRPCEmbeddingBuilder returns a new instance of GRPCEmbeddingBuilder with the given VectorSearchConfig
+// Parameters:
+// - cfg: A pointer to a VectorSearchConfig object
+// Returns:
+// - A pointer to a GRPCEmbeddingBuilder object
+// Example Usage:
+//
+//	cfg := &VectorSearchConfig{
+//	    VectorSearchHost: "localhost",
+//	    VectorSearchPort: 8080,
+//	}
+//
+// builder := NewGRPCEmbeddingBuilder(cfg)
 func NewGRPCEmbeddingBuilder(cfg *VectorSearchConfig) *GRPCEmbeddingBuilder {
 	return &GRPCEmbeddingBuilder{
 		cfg: cfg,
 		mx:  sync.Mutex{},
 	}
 }
+
+// Client returns a SearchServiceClient instance for making search requests.
+// If the client is not already initialized, it creates a new connection
+// to the VectorSearch service using the provided configuration and sets the client.
+// It returns the client and an error if any occurred during initialization.
 func (e *GRPCEmbeddingBuilder) Client() (proto.SearchServiceClient, error) {
 	e.mx.Lock()
 	defer e.mx.Unlock()
--- src/backend/core/ai/searcher_grpc.go
+++ src/backend/core/ai/searcher_grpc.go
@@ -7,13 +7,27 @@ import (
 	"go.uber.org/zap"
 )
 
-type SearcherGrpc struct {
-}
-
+// SearcherGRPC is a type that represents a searcher implementation using gRPC.
+// It contains an embeddBuilder field of type *GRPCEmbeddingBuilder, which is
+// responsible for constructing the gRPC embedding functionality.
 type SearcherGRPC struct {
 	embeddBuilder *GRPCEmbeddingBuilder
 }
 
+// FindDocuments searches for documents based on the provided criteria.
+// It uses gRPC embedding functionality to perform the search.
+//
+// Parameters:
+//   - ctx: The context.Context for the search operation.
+//   - userID: The ID of the user.
+//   - tenantID: The ID of the tenant.
+//   - embeddingModel: The embedding model to use for the search.
+//   - message: The message to search for.
+//   - collectionNames: The optional collection names to search within.
+//
+// Returns:
+//   - []*SearcherResponse: A slice of SearcherResponse objects representing the search results.
+//   - error: An error if any occurred during the search.
 func (i *SearcherGRPC) FindDocuments(ctx context.Context, userID, tenantID uuid.UUID,
 	embeddingModel string,
 	message string, collectionNames ...string) ([]*SearcherResponse, error) {

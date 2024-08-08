2d891e1499c9fd54dfa0c87437d6e822ffbfbf16
aPaladiychuk <andrey.paladiychuk@gmail.com>
2024-08-08 15:01:24 +0300

    add documentation

--- src/backend/configmap/docker.go
+++ src/backend/configmap/docker.go
@@ -10,11 +10,15 @@ import (
 	"strings"
 )
 
+// DockerServer represents a server for Docker configuration maps.
 type DockerServer struct {
 	proto.UnsafeConfigMapServer
 	path string
 }
 
+// readConfig reads the contents of the specified file and returns them as a slice of strings.
+// The function removes any carriage return characters (\r) from the file contents before splitting it into rows.
+// It returns the rows as a slice of strings and an error if the file cannot be read.
 func (k *DockerServer) readConfig(name string) ([]string, error) {
 	buf, err := os.ReadFile(name)
 	if err != nil {
@@ -26,6 +30,13 @@ func (k *DockerServer) readConfig(name string) ([]string, error) {
 	return rows, nil
 
 }
+
+// GetList retrieves a list of config map records from the DockerServer.
+// It takes a context and a ConfigMapList as input and returns a ConfigMapListResponse and an error.
+// The method reads the config file for the specified ConfigMapList name and populates the response
+// with the values found in the file. Each line in the file is treated as a key-value pair separated by "=".
+// If the line does not contain exactly two elements, it is skipped. The response is then returned.
+// If an error occurs while reading the file, it is propagated back to the caller.
 func (k *DockerServer) GetList(ctx context.Context, r *proto.ConfigMapList) (*proto.ConfigMapListResponse, error) {
 	filename := fmt.Sprintf("%s/%s.env", k.path, r.Name)
 
@@ -50,6 +61,14 @@ func (k *DockerServer) GetList(ctx context.Context, r *proto.ConfigMapList) (*pr
 	return &result, nil
 }
 
+// Save saves the configuration map to a file.
+// It takes a context and a ConfigMapSave request as input.
+// It retrieves the filename based on the path and name from the request.
+// It reads the existing configuration from the file.
+// If the key already exists in the configuration, it updates the value.
+// If the key doesn't exist, it adds a new key-value pair.
+// Finally, it writes the updated configuration to the file.
+// It returns an empty message and any error that occurred.
 func (k *DockerServer) Save(ctx context.Context, r *proto.ConfigMapSave) (*empty.Empty, error) {
 	filename := fmt.Sprintf("%s/%s.env", k.path, r.Name)
 	rows, err := k.readConfig(filename)
@@ -74,6 +93,7 @@ func (k *DockerServer) Save(ctx context.Context, r *proto.ConfigMapSave) (*empty
 	return &empty.Empty{}, os.WriteFile(filename, []byte(strings.Join(rows, "\n")), 0644)
 }
 
+// Delete removes a key-value pair from a config map file.
 func (k *DockerServer) Delete(ctx context.Context, r *proto.ConfigMapDelete) (*empty.Empty, error) {
 	filename := fmt.Sprintf("%s/%s.env", k.path, r.Name)
 	rows, err := k.readConfig(filename)
@@ -92,6 +112,12 @@ func (k *DockerServer) Delete(ctx context.Context, r *proto.ConfigMapDelete) (*e
 	return &empty.Empty{}, os.WriteFile(filename, []byte(strings.Join(newRows, "\n")), 0644)
 }
 
+// NewDockerServer creates a new instance of DockerServer with the specified root path.
+// It implements the proto.ConfigMapServer interface and returns the server instance and an error.
+// If the root path does not exist, it returns an error indicating that the docker config maps
+// cannot be found.
+//
+// NewDockerServer(root string) (proto.ConfigMapServer, error)
 func NewDockerServer(root string) (proto.ConfigMapServer, error) {
 	if _, err := os.Stat(root); os.IsNotExist(err) {
 		return nil, fmt.Errorf("can not find docker config maps")
--- src/backend/configmap/k8s.go
+++ src/backend/configmap/k8s.go
@@ -10,14 +10,66 @@ import (
 	"os"
 )
 
+// nsSecret is a constant representing the file path to the Kubernetes Service Account Namespace Secret.
 const nsSecret = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 
+// K8SServer represents a Kubernetes server with additional methods for ConfigMap operations.
+//
+// It embeds the proto.UnsafeConfigMapServer interface and includes the following fields:
+// - namespace: the namespace in which the ConfigMaps are managed
+// - client: a Kubernetes clientset used to interact with the Kubernetes cluster
+//
+// The K8SServer type provides the following methods:
+// - GetList: retrieves the list of ConfigMaps in the namespace
+// - Save: saves a ConfigMap to the namespace
+// - Delete: deletes a ConfigMap from the namespace
+//
+// Example Usage:
+//
+// server, err := NewK8SServer()
+//
+//	if err != nil {
+//	  // handle error
+//	}
+//
+// ctx := context.Background()
+// listReq := &proto.ConfigMapList{Name: "example"}
+// listResp, err := server.GetList(ctx, listReq)
+//
+//	if err != nil {
+//	  // handle error
+//	}
+//
+//	saveReq := &proto.ConfigMapSave{
+//	  Name: "example",
+//	  Value: &proto.ConfigMapValue{
+//	    Key:   "key",
+//	    Value: "value",
+//	  },
+//	}
+//
+// _, err = server.Save(ctx, saveReq)
+//
+//	if err != nil {
+//	  // handle error
+//	}
+//
+// deleteReq := &proto.ConfigMapDelete{Name: "example", Key: "key"}
+// _, err = server.Delete(ctx, deleteReq)
+//
+//	if err != nil {
+//	  // handle error
+//	}
 type K8SServer struct {
 	proto.UnsafeConfigMapServer
 	namespace string
 	client    *kubernetes.Clientset
 }
 
+// GetList retrieves a list of config maps based on the provided ConfigMapList request.
+// The method first fetches the config map identified by the name in the request.
+// It then constructs a ConfigMapListResponse containing the key-value pairs of the config map.
+// Finally, it returns the resulting ConfigMapListResponse or an error if any occurred.
 func (k *K8SServer) GetList(ctx context.Context, r *proto.ConfigMapList) (*proto.ConfigMapListResponse, error) {
 	configMap, err := k.client.CoreV1().ConfigMaps(k.namespace).Get(ctx, r.GetName(), v1.GetOptions{})
 	if err != nil {
@@ -35,6 +87,7 @@ func (k *K8SServer) GetList(ctx context.Context, r *proto.ConfigMapList) (*proto
 	return &result, nil
 }
 
+// Save saves the value of a specific key in a ConfigMap.
 func (k *K8SServer) Save(ctx context.Context, r *proto.ConfigMapSave) (*empty.Empty, error) {
 	configMap, err := k.client.CoreV1().ConfigMaps(k.namespace).Get(ctx, r.GetName(), v1.GetOptions{})
 	if err != nil {
@@ -47,6 +100,14 @@ func (k *K8SServer) Save(ctx context.Context, r *proto.ConfigMapSave) (*empty.Em
 	return &empty.Empty{}, nil
 }
 
+// Delete deletes a data entry from a ConfigMap.
+//
+// The Delete method retrieves the ConfigMap with the given name using the specified
+// namespace and gets the data entry corresponding to the specified key. It deletes
+// the entry from the ConfigMap by removing it from the map. Finally, it updates the
+// ConfigMap in the Kubernetes cluster with the updated data and returns an empty
+// response if the operation is successful, or an error if any error occurs while
+// performing the deletion or update.
 func (k *K8SServer) Delete(ctx context.Context, r *proto.ConfigMapDelete) (*empty.Empty, error) {
 	configMap, err := k.client.CoreV1().ConfigMaps(k.namespace).Get(ctx, r.GetName(), v1.GetOptions{})
 	if err != nil {
@@ -60,6 +121,14 @@ func (k *K8SServer) Delete(ctx context.Context, r *proto.ConfigMapDelete) (*empt
 	return &empty.Empty{}, nil
 }
 
+// NewK8SServer returns a new instance of K8SServer that implements the ConfigMapServer interface and
+// communicates with Kubernetes API to perform CRUD operations on ConfigMap resources.
+//
+// The function first retrieves the in-cluster configuration using rest.InClusterConfig().
+// Then it creates a new Kubernetes clientset using the obtained configuration.
+// It then calls the getCurrentNamespace() function to get the current namespace that the server will operate on.
+// Finally, it creates a new instance of K8SServer with the client and namespace, and returns it along with any error encountered.
+// If any error occurs during the process, nil is returned as the server instance and the error is propagated.
 func NewK8SServer() (proto.ConfigMapServer, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -80,6 +149,9 @@ func NewK8SServer() (proto.ConfigMapServer, error) {
 	}, nil
 }
 
+// getCurrentNamespace retrieves the current Kubernetes namespace by reading the contents
+// of the nsSecret file. If an error occurs while reading the file, an empty string and
+// the error are returned.
 func getCurrentNamespace() (string, error) {
 	ns, err := os.ReadFile(nsSecret)
 	if err != nil {
